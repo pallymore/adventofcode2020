615afa688ad065c305022b8b7b8a38e1c00d7d92
Yurui Zhang <yuruiology@gmail.com>
2021-01-05 23:57:33 -0500

    cleans up day 21 solution

---
+++ 21/part_2.js
@@ -0,0 +1,69 @@
+const readline = require("readline");
+const fs = require("fs");
+
+solution().then(console.log);
+
+async function solution() {
+  const inputStream = fs.createReadStream("./input");
+
+  const lines = readline.createInterface({
+    input: inputStream,
+    crlfDelay: Infinity,
+  });
+
+  const ingredientsCount = {}; // ingredient -> count
+  const possibleAllergicIngredients = {}; // allergen -> ingredient[]
+
+  for await (const line of lines) {
+    const [i, a] = line.split(" (contains ");
+    const ingredients = i.split(" ");
+    const allergens = a.split(", ").map((allergen) => {
+      return allergen.replace(/\)/g, "");
+    });
+
+    ingredients.forEach((ing) => {
+      ingredientsCount[ing] = (ingredientsCount[ing] ?? 0) + 1;
+    });
+
+    allergens.forEach((al) => {
+      if (!possibleAllergicIngredients[al]) {
+        // has not seen this before, assume everything is allergic
+        possibleAllergicIngredients[al] = ingredients;
+      } else {
+        // filter out the ones that are not present in the current ingredients list
+        possibleAllergicIngredients[al] = possibleAllergicIngredients[
+          al
+        ].filter((ingredient) => ingredients.includes(ingredient));
+      }
+    });
+  }
+
+  const allergenIngredient = {}; // allergen -> ingredient
+  while (Object.keys(possibleAllergicIngredients).length > 0) {
+    // find a record that has only 1 ingredient
+    const definitelyAllergic = Object.keys(possibleAllergicIngredients).find(
+      (k) => possibleAllergicIngredients[k].length <= 1
+    );
+    if (!definitelyAllergic) {
+      // prevent infinite loop;
+      throw new Error("cannot find anything that is definitely allergic");
+    }
+    const ingredient = possibleAllergicIngredients[definitelyAllergic][0];
+    // add it to our known list
+    allergenIngredient[definitelyAllergic] = ingredient;
+    // remove it from possible allergic ingredients map;
+    delete possibleAllergicIngredients[definitelyAllergic];
+    // remove that ingredient from all other possibilities
+    Object.keys(possibleAllergicIngredients).forEach((k) => {
+      possibleAllergicIngredients[k] = possibleAllergicIngredients[k].filter(
+        (ing) => ing !== ingredient
+      );
+    });
+  }
+
+  const dangerList = Object.keys(allergenIngredient)
+    .sort()
+    .map((allergen) => allergenIngredient[allergen]);
+
+  return dangerList.join(",");
+}
